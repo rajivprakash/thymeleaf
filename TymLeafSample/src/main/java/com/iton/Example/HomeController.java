582d72c2bc5b62a835f0a7ac9101b769472c4fd7
Rajiv Prakash <rajivprakash1424@gmail.com>
2015-07-01 11:50:10 +0530

    Initial Commit

---
+++ TymLeafSample/src/main/java/com/iton/Example/HomeController.java
@@ -0,0 +1,42 @@
+package com.iton.Example;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = {"/","/Home"}, method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		System.out.println("Welcome home! The client locale is {}."+locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	@RequestMapping(value="/Contact")
+	public String contactPage()
+	{
+		
+		
+		return "Contact";
+	}
+}
